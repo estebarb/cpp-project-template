f68e2bbac00aeed520a41dbd38072b91f08b76fa
Esteban Rodriguez Betancourt <estebarb@gmail.com>
2024-06-10 23:50:04 -0600

    Fix ut errors

--- tests/test_fibonacci.cpp
+++ tests/test_fibonacci.cpp
@@ -3,3 +3,9 @@
 #include <fibonacci.hpp>
 
 TEST(Fibonacci, FibonacciZero) { EXPECT_EQ(0, fibonacci(0)); }
+
+TEST(Fibonacci, FibonacciOne) { EXPECT_EQ(1, fibonacci(1)); }
+
+TEST(Fibonacci, FibonacciTwo) { EXPECT_EQ(1, fibonacci(2)); }
+
+TEST(Fibonacci, FibonacciThree) { EXPECT_EQ(2, fibonacci(3)); }
